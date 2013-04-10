a89b8ab8450dc5bade7da0e242e4a3eafd55139a
Fourure Damien <Damien@wifi-etu-0131.univ-lyon1.fr>
2013-04-10 09:22:50 +0200

    Initial Commit

---
+++ Image Analysis Project/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Image Analysis Project
+//
+//  Created by Fourure Damien on 10/04/13.
+//  Copyright (c) 2013 Fourure Damien, Kong Win Chang Bryan, Tavernier Rémi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
